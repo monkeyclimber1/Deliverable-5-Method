f4e8f74785324d071df3e55c57d83a690cad0c31
Bryan <Bryan@DESKTOP-9CJ4E44>
2022-10-03 20:58:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Deliverable_5_Method
+{
+    internal class Program
+    {
+              
+        static int sum(int[] Array)
+        {
+            int MySum = Array.Sum();
+
+            return MySum;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter an integer number between 5 and 15: ");
+                int input = int.Parse(Console.ReadLine());
+
+                if (input >= 5 & input <= 15)
+                {
+                    int[] MyArray = random_array(input);
+
+                    Console.Write("The elements in the random array are: ");
+
+                    foreach (int i in MyArray)
+                    {
+                        Console.Write(i + " ");
+                    }
+
+                    Console.WriteLine("\nThe sum is: " + sum(MyArray));
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an integer within the range 5 and 15");
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Enter an integer data type");
+
+            }
+
+        }
+        static int[] random_array(int array_len)
+        {
+            Random random = new Random();
+            int[] MyArray = new int[array_len];
+            for (int i = 0; i < array_len; i++)
+            {
+                MyArray[i] = random.Next(10, 50);
+            }
+
+            return MyArray;
+        }
+    }
+}
+
+
